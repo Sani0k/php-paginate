4aa227de25d9b963a53208a961c3a4dccf658286
hjpotter92 <hjpotter92@iitkgp.ac.in>
2013-06-24 02:08:05 +0530

    setOption method
     - Store options as array.
    - class method to set options later

--- paginator.php
+++ paginator.php
@@ -1,25 +1,35 @@
 <?php
 	class Paginator {
-		private $currentPage, $lastPage, $firstPage = 1, $scope = 3;
+		private $currentPage, $lastPage;
+		private $options = Array(
+			'firstPage' => 1,
+			'scope' => 3,
+		);
 		private $start, $goal, $maxrange;
 		
-		private function Paginate() {
-			$this->currentPage = ($this->currentPage < $this->firstPage) ? $this->firstPage : $this->currentPage;
-			$this->start = ($this->lastPage > $this->maxrange) ? max($this->firstPage, $this->currentPage - $this->scope) : $this->firstPage;
-			$this->goal = ($this->lastPage > $this->maxrange) ? min($this->lastPage, $this->currentPage + $this->scope) : $this->lastPage;
-		}
-		
-		function __construct( $total, $present = 1 ) {
+		function __construct( $total, $present = 1, $options = Array() ) {
+			$options = array_merge( $this->options, $options );
 			$this->currentPage = $present;
 			$this->lastPage = $total;
-			$this->maxrange = $this->scope * 2 - 1;
-			$this->Paginate();
+		}
+		
+		public function setOption( $literal, $value ) {
+			if( isset($this->options[$literal]) ) {
+				$this->options[$literal] = $value;
+			}
+		}
+		
+		public function Paginate() {
+			$this->maxrange = $options['scope'] * 2 - 1;
+			$this->currentPage = ($this->currentPage < $this->options['firstPage']) ? $this->options['firstPage'] : $this->currentPage;
+			$this->start = ($this->lastPage > $this->maxrange) ? max($this->options['firstPage'], $this->currentPage - $this->options['scope']) : $this->options['firstPage'];
+			$this->goal = ($this->lastPage > $this->maxrange) ? min($this->lastPage, $this->currentPage + $this->options['scope']) : $this->lastPage;
 		}
 		
 		public function Generate() {
 			$element = "<li style='display: inline; padding: 0px 2px;'><span>%d</span></li>";
 			echo "<ul style='display: inline-block;'>";
-			if( $this->start == $this->firstPage ) {
+			if( $this->start == $this->options['firstPage'] ) {
 				for( $i = $this->start; $i <= $this->goal; $i++ ) {
 					echo sprintf( $element, $i ) . "\n";
 				}
@@ -28,7 +38,7 @@
 				}
 			}
 			else {
-				echo sprintf( $element, $this->firstPage ) . "\n";
+				echo sprintf( $element, $this->options['firstPage'] ) . "\n";
 				for( $i = $this->start; $i <= $this->goal; $i++ ) {
 					echo sprintf( $element, $i ) . "\n";
 				}
