b9834dae2a82dd5a36ea788a7d93d210cbad4971
hjpotter92 <hjpotter92@iitkgp.ac.in>
2013-06-25 02:20:30 +0530

    Cast variables to int
     - Casting variables
    - Flag for pagination

--- paginator.php
+++ paginator.php
@@ -1,16 +1,22 @@
 <?php
 	class Paginator {
-		private $currentPage, $lastPage;
+		private $currentPage, $lastPage, $start, $goal, $maxrange;
 		private $options = Array(
 			'firstPage' => 1,
 			'scope' => 3,
 		);
-		private $start, $goal, $maxrange;
+		private $isPaginated = FALSE;
 		
 		function __construct( $total, $present = 1, $options = Array() ) {
-			$options = array_merge( $this->options, $options );
-			$this->currentPage = $present;
-			$this->lastPage = $total;
+			$this->options = array_merge( $this->options, $options );
+			$this->currentPage = (int) $present;
+			$this->lastPage = (int) $total;
+		}
+		
+		private function Logic() {
+			$this->maxrange = (int) ($this->options['scope'] * 2 - 1);
+			$this->options['firstPage'] = (int) ($this->options['firstPage'] < $this->lastPage) ? $this->options['firstPage'] : 1;
+			$this->currentPage = (int) ($this->currentPage < $this->options['firstPage']) ? $this->options['firstPage'] : $this->currentPage;
 		}
 		
 		public function setOption( $literal, $value ) {
@@ -20,13 +26,16 @@
 		}
 		
 		public function Paginate() {
-			$this->maxrange = $options['scope'] * 2 - 1;
-			$this->currentPage = ($this->currentPage < $this->options['firstPage']) ? $this->options['firstPage'] : $this->currentPage;
-			$this->start = ($this->lastPage > $this->maxrange) ? max($this->options['firstPage'], $this->currentPage - $this->options['scope']) : $this->options['firstPage'];
-			$this->goal = ($this->lastPage > $this->maxrange) ? min($this->lastPage, $this->currentPage + $this->options['scope']) : $this->lastPage;
+			$this->Logic();
+			$this->start = (int) ($this->lastPage > $this->maxrange) ? max($this->options['firstPage'], $this->currentPage - $this->options['scope']) : $this->options['firstPage'];
+			$this->goal = (int) ($this->lastPage > $this->maxrange) ? min($this->lastPage, $this->currentPage + $this->options['scope']) : $this->lastPage;
+			$this->isPaginated = TRUE;
 		}
 		
 		public function Generate() {
+			if( $this->isPaginated !== TRUE ) {
+				throw new Exception( "You need to call Paginator::Paginate() before generating results." );
+			}
 			$element = "<li style='display: inline; padding: 0px 2px;'><span>%d</span></li>";
 			echo "<ul style='display: inline-block;'>";
 			if( $this->start == $this->options['firstPage'] ) {
